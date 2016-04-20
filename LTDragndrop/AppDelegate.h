3a2a9193340ffd1d6ce33100ec257c5bff5b14c4
Pavel Razuvaev <p.razuvaev@ltst.ru>
2016-04-20 09:38:53 +0600

    Initial Commit

---
+++ LTDragndrop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTDragndrop
+//
+//  Created by Pavel Razuvaev on 20/04/16.
+//  Copyright © 2016 Pavel Razuvaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LTDragndrop/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LTDragndrop
+//
+//  Created by Pavel Razuvaev on 20/04/16.
+//  Copyright © 2016 Pavel Razuvaev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
